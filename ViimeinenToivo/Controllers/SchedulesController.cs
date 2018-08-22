d3b67ee5109675ccde322ec3cdda36fd38a39209
npuh <ninapuhakka@hotmail.com>
2018-08-22 13:47:14 +0300

    Controllers - program controller

---
+++ ViimeinenToivo/Controllers/SchedulesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViimeinenToivo.Models;
+
+namespace ViimeinenToivo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchedulesController : ControllerBase
+    {
+        private readonly ViikkoProjektiDBContext _context;
+
+        public SchedulesController(ViikkoProjektiDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Schedules
+        [HttpGet]
+        public IEnumerable<Schedule> GetSchedule()
+        {
+            return _context.Schedule;
+        }
+
+        // GET: api/Schedules/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSchedule([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var schedule = await _context.Schedule.FindAsync(id);
+
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(schedule);
+        }
+
+        // PUT: api/Schedules/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSchedule([FromRoute] int id, [FromBody] Schedule schedule)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != schedule.ScheduleId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(schedule).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ScheduleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Schedules
+        [HttpPost]
+        public async Task<IActionResult> PostSchedule([FromBody] Schedule schedule)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Schedule.Add(schedule);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSchedule", new { id = schedule.ScheduleId }, schedule);
+        }
+
+        // DELETE: api/Schedules/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSchedule([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var schedule = await _context.Schedule.FindAsync(id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            _context.Schedule.Remove(schedule);
+            await _context.SaveChangesAsync();
+
+            return Ok(schedule);
+        }
+
+        private bool ScheduleExists(int id)
+        {
+            return _context.Schedule.Any(e => e.ScheduleId == id);
+        }
+    }
+}
