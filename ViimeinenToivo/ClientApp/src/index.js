bb82922f3b3d8e041a6bbef5770f62bb851fa474
tatuvahteri <tatu.vahteri@gmail.com>
2018-08-22 11:53:14 +0300

    Auth0 lisätty onnistuneesti (?)

--- ViimeinenToivo/ClientApp/src/index.js
+++ ViimeinenToivo/ClientApp/src/index.js
@@ -1,19 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { makeMainRoutes } from './routes';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+const routes = makeMainRoutes();
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
-
-registerServiceWorker();
+    routes,
+    document.getElementById('root')
+);
